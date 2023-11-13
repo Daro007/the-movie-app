ba4012bdb17819aa8b735a29f722fbf32d805e1d
dario elias <darioelias@darios-iMac.local>
2023-11-13 11:12:09 +0100

    Fix UI bug on the Card component

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -4,6 +4,13 @@ import Link from "next/link";
 import { MovieCardProps } from "../types";
 
 const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
+
+  const maxTitleLength = 30;
+  const truncatedTitle =
+    movie.title.length > maxTitleLength
+      ? `${movie.title.substring(0, maxTitleLength)}...`
+      : movie.title;
+
   return (
     <Link
       aria-label="Browse Movies"
@@ -11,7 +18,7 @@ const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
     >
       <div className="carousel-item card w-96 bg-base-100 shadow-xl m-4">
         <div className="card-body">
-          <h3 className="card-title">{movie.title}</h3>
+          <h3 className="card-title">{truncatedTitle}</h3>
           <p>{movie.release_date}</p>
         </div>
         <figure>
